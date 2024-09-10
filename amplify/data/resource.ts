ef999fc9ebf216cb45cbbff666ef798d3fb1d873
Mohamed Asmali <asmali@Mohameds-MacBook-Pro.local>
2024-09-10 11:29:16 -0400

    added storage

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,22 +1,12 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { defineStorage } from "@aws-amplify/backend";
 
-const schema = a.schema({
-  Note: a
-    .model({
-      name:a.string(),
-      description: a.string(),
-      image: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
-  },
+export const storage = defineStorage({
+  name: "amplifyNotesDrive",
+  access: (allow) => ({
+    "media/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+  }),
 });
 
 /*== STEP 2 ===============================================================
